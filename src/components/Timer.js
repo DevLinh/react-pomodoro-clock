26cb7a79365f70eac1defb926561ba32b1386b30
DevLinh <DevLinh99QB@gmail.com>
2019-11-12 11:42:31 +0700

    add timer handle control

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,12 +1,84 @@
 import React, { Component } from "react";
 import TimerLengthControl from "./TimerLengthControl";
 export class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brkLength: 5,
+      sesLength: 15,
+      timerState: "stopped", // stopped or running
+      timerType: "Session",
+      timer: 900,
+      intervalID: "",
+      alarmColor: { color: "white" }
+    };
+    this.lengthControl = this.lengthControl.bind(this);
+    this.setBrkLength = this.setBrkLength.bind(this);
+    this.setSesLength = this.setSesLength.bind(this);
+  }
+
+  //lengthControl for Break Length and Session Length
+  lengthControl(stateToChange, sign, currentLength, timerType) {
+    if (this.state.timerType !== timerType) {
+      if (sign === "-" && currentLength !== 1) {
+        this.setState({ [stateToChange]: currentLength - 1 });
+      } else if (sign === "+" && currentLength !== 60) {
+        this.setState({ [stateToChange]: currentLength + 1 });
+      }
+    } else {
+      if (sign === "-" && currentLength !== 1) {
+        this.setState({
+          [stateToChange]: currentLength - 1,
+          timer: currentLength * 60 - 60 // count down when it remains 60s
+        });
+      } else if (sign === "+" && currentLength !== 60) {
+        this.setState({
+          [stateToChange]: currentLength + 1,
+          timer: currentLength * 60 - 60
+        });
+      }
+    }
+  }
+  setBrkLength(e) {
+    this.lengthControl(
+      "brkLength",
+      e.currentTarget.value,
+      this.state.brkLength,
+      "Break"
+    );
+    console.log(this.state.timer);
+  }
+  setSesLength(e) {
+    this.lengthControl(
+      "sesLength",
+      e.currentTarget.value,
+      this.state.sesLength,
+      "Session"
+    );
+    console.log(this.state.timer);
+  }
   render() {
     return (
       <div>
         <div className="main-title">Pomodoro Clock</div>
-        <TimerLengthControl title="Break Length" length="000" />
-        <TimerLengthControl title="Session Length" length="111" />
+        <TimerLengthControl
+          title="Break Length"
+          titleID="break-label"
+          minID="break-decrement"
+          addID="break-increment"
+          lengthID="break-length"
+          length={this.state.brkLength}
+          onClick={this.setBrkLength}
+        />
+        <TimerLengthControl
+          title="Session Length"
+          titleID="session-label"
+          minID="session-decrement"
+          addID="session-increment"
+          lengthID="session-length"
+          length={this.state.sesLength}
+          onClick={this.setSesLength}
+        />
       </div>
     );
   }
--- src/components/TimerLengthControl.js
+++ src/components/TimerLengthControl.js
@@ -3,14 +3,24 @@ import "../../node_modules/font-awesome/css/font-awesome.min.css";
 export class TimerLengthControl extends Component {
   render() {
     return (
-      <div>
-        <div>{this.props.title}</div>
-        <button>
+      <div className="length-control">
+        <div id={this.props.titleID}>{this.props.title}</div>
+        <button
+          id={this.props.minID}
+          className="btn-level"
+          value="-"
+          onClick={this.props.onClick}
+        >
           <i className="fa fa-arrow-down fa-2x" />
         </button>
-        <div>{this.props.length}</div>
-        <button>
-          <i className="fa fa-arrow-down fa-2x" />
+        <div id={this.props.lengthID}>{this.props.length}</div>
+        <button
+          id={this.props.addID}
+          className="btn-level"
+          value="+"
+          onClick={this.props.onClick}
+        >
+          <i className="fa fa-arrow-up fa-2x" />
         </button>
       </div>
     );
